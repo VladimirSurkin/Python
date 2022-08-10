211dc29b16798580f8db11d734a4702268947a17
VladimirSurkin <108413248+VladimirSurkin@users.noreply.github.com>
2022-08-10 09:10:55 +0300

    Delete Task_05.py

--- Task_05.py
+++
@@ -1,6 +0,0 @@
-# 5
-from functools import reduce
-
-list = [i for i in range(100, 1001, 2)]
-print("Список чётных чисел в диапазоне [100..1000]: ", list)
-print("Произведение всех элементов списка: ", reduce(lambda x, y: x * y, list))
